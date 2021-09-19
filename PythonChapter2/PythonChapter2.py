d37ef2e8f75ca14f36237335c25843d53ad9cc7c
Phil Benford <phillipbenford@gmail.com>
2021-09-19 12:29:37 +0100

    complete

--- PythonChapter2/PythonChapter2.py
+++ PythonChapter2/PythonChapter2.py
@@ -1,1 +1,46 @@
+# LAB 1
+print("Hello,World!")
+print("Phil")
+# error: print(Phil)
+# error: print"Phil"
 
+#  LAB 2
+print("Programming","Essentials","in", sep="***", end="...")
+print("Python")
+
+# LAB 3
+print("    *\n   * *\n  *   *\n *     *\n***   ***\n  *   *\n  *   *\n  *****")
+
+print("        *")
+print("       * *")
+print("      *   *")
+print("     *     *")
+print("    *       *")
+print("   *         *")
+print("  *           *")
+print(" *             *")
+print("******     ******")
+print("     *     *")
+print("     *     *")
+print("     *     *")
+print("     *     *")
+print("     *     *")
+print("     *     *")
+print("     *******")
+
+print("         *         "*2)
+print("        * *        "*2)
+print("       *   *       "*2)
+print("      *     *      "*2)
+print("     *       *     "*2)
+print("    *         *    "*2)
+print("   *           *   "*2)
+print("  *             *  "*2)
+print(" ******     ****** "*2)
+print("      *     *      "*2)
+print("      *     *      "*2)
+print("      *     *      "*2)
+print("      *     *      "*2)
+print("      *     *      "*2)
+print("      *     *      "*2)
+print("      *******      "*2)
