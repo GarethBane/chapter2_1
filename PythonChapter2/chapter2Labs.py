ce7f765d135cc066a882ebda837f6fa0704dae4c
Phil Benford <phillipbenford@gmail.com>
2021-09-20 12:56:44 +0100

    commit

--- PythonChapter2/chapter2Labs.py
+++ PythonChapter2/chapter2Labs.py
@@ -1,54 +1,107 @@
-# CHAPTER 2.1
+############################### CHAPTER 2.1
 
-# LAB 1 
-print("Hello,World!")
-print("Phil")
-# error: print(Phil)
-# error: print"Phil"
+    # LAB 1 
+        print("Hello,World!")
+        print("Phil")
+        # error: print(Phil)
+        # error: print"Phil"
 
-#  LAB 2
-print("Programming","Essentials","in", sep="***", end="...")
-print("Python")
+    #  LAB 2
+        print("Programming","Essentials","in", sep="***", end="...")
+        print("Python")
 
-# LAB 3
-print("    *\n   * *\n  *   *\n *     *\n***   ***\n  *   *\n  *   *\n  *****")
+    # LAB 3
+        print("    *\n   * *\n  *   *\n *     *\n***   ***\n  *   *\n  *   *\n  *****")
 
-print("        *")
-print("       * *")
-print("      *   *")
-print("     *     *")
-print("    *       *")
-print("   *         *")
-print("  *           *")
-print(" *             *")
-print("******     ******")
-print("     *     *")
-print("     *     *")
-print("     *     *")
-print("     *     *")
-print("     *     *")
-print("     *     *")
-print("     *******")
+        print("        *")
+        print("       * *")
+        print("      *   *")
+        print("     *     *")
+        print("    *       *")
+        print("   *         *")
+        print("  *           *")
+        print(" *             *")
+        print("******     ******")
+        print("     *     *")
+        print("     *     *")
+        print("     *     *")
+        print("     *     *")
+        print("     *     *")
+        print("     *     *")
+        print("     *******")
 
-print("         *         "*2)
-print("        * *        "*2)
-print("       *   *       "*2)
-print("      *     *      "*2)
-print("     *       *     "*2)
-print("    *         *    "*2)
-print("   *           *   "*2)
-print("  *             *  "*2)
-print(" ******     ****** "*2)
-print("      *     *      "*2)
-print("      *     *      "*2)
-print("      *     *      "*2)
-print("      *     *      "*2)
-print("      *     *      "*2)
-print("      *     *      "*2)
-print("      *******      "*2)
+        print("         *         "*2)
+        print("        * *        "*2)
+        print("       *   *       "*2)
+        print("      *     *      "*2)
+        print("     *       *     "*2)
+        print("    *         *    "*2)
+        print("   *           *   "*2)
+        print("  *             *  "*2)
+        print(" ******     ****** "*2)
+        print("      *     *      "*2)
+        print("      *     *      "*2)
+        print("      *     *      "*2)
+        print("      *     *      "*2)
+        print("      *     *      "*2)
+        print("      *     *      "*2)
+        print("      *******      "*2)
 
 
-# CHAPTER 2.2
+############################### CHAPTER 2.2
 
-# LAB 1
-print("\"I'm\"\n\"\"learning\"\"\n\"\"\"Python\"\"\"")
+    # LAB 1
+        print("\"I'm\"\n\"\"learning\"\"\n\"\"\"Python\"\"\"")
+
+############################### CHAPTER 2.3
+
+    # Nil LAB
+
+############################### CHAPTER 2.4
+
+    # LAB 1: VARIABLES
+
+        john = 3
+        mary = 5 
+        adam = 6
+
+        # Cant use + to join opposing literal ie integer + string. However using a comma in in this scenario produces the desired outcome.
+        print(john, ",", mary, ",", adam)
+
+        total_apples = john + mary + adam
+
+        print(total_apples)
+
+        print("Total number of apples:", total_apples)
+
+    # LAB 2: Variables: a simple converter
+
+        kilometers = 12.25
+        miles = 7.38
+
+        miles_to_kilometers = miles * 1.61
+        kilometers_to_miles = kilometers / 1.61
+
+        print(miles, "miles is", round(miles_to_kilometers, 2), "kilometers")
+        print(kilometers, "kilometers is", round(kilometers_to_miles, 2), "miles")
+
+    # LAB 3: Operators and expressions
+        
+        x = 0
+        x = float(x)
+        y = 3 * x ** 3 - 2 * x ** 2 + 3 * x - 1
+        print("y =", y)
+
+        x = 1
+        x = float(x)
+        y = 3 * x ** 3 - 2 * x ** 2 + 3 * x - 1
+        print("y =", y)
+
+        x = -1
+        x = float(x)
+        y = 3 * x ** 3 - 2 * x ** 2 + 3 * x - 1
+        print("y =", y)
+
+
+
+############################### CHAPTER 2.5
