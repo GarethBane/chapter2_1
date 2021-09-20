132a125986e6629b5d2b5143e2a8408ec683a765
Phil Benford <phillipbenford@gmail.com>
2021-09-20 16:12:51 +0100

    Commit

--- PythonChapter2/chapter2Labs.py
+++ PythonChapter2/chapter2Labs.py
@@ -105,3 +105,30 @@
 
 
 ############################### CHAPTER 2.5
+
+    # LAB 1: Comments
+
+    # Before editing - Original code
+
+        #this program computes the number of seconds in a given number of hours
+        # this program has been written two days ago
+
+        a = 2 # number of hours
+        seconds = 3600 # number of seconds in 1 hour
+
+        print("Hours: ", a) #printing the number of hours
+        # print("Seconds in Hours: ", a * seconds) # printing the number of seconds in a given number of hours
+
+        #here we should also print "Goodbye", but a programmer didn't have time to write any code
+        #this is the end of the program that computes the number of seconds in 3 hour
+
+
+    # After editing
+
+        #this program computes the number of seconds in a given number of hours
+        hours = 2 # number of hours
+        seconds = 60 # number of seconds in 1 hour
+
+        print("Hours:", hours) #printing the number of hours
+        print("Seconds in Hours:", hours * seconds) # printing the number of seconds in a given number of hours
+        print("Goodbye")
