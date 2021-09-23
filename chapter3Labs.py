59572d3ffbe8bb71efbb18c759a263bb6c0d961f
phillip benford <phillipbenford@gmail.com>
2021-09-23 11:02:43 +0100

    commit

--- chapter3Labs.py
+++ chapter3Labs.py
@@ -143,3 +143,25 @@ else:
     # Step 3: write a line of code that prints the length of the existing list.
     print(len(hat_list))
     print(hat_list)
+
+    # LAB: 3.4.1.13 The basics of lists - the Beatles
+    # step 1
+    beatles = []
+    print("Step 1:", beatles)
+
+    # step 2
+    beatles.append["John Lennon", "Paul McCartney", "George Harrison"]
+    print("Step 2:", beatles)
+
+    # step 3
+    print("Step 3:", beatles)
+
+    # step 4
+    print("Step 4:", beatles)
+
+    # step 5
+    print("Step 5:", beatles)
+
+
+    # testing list legth
+    print("The Fab", len(beatles))
