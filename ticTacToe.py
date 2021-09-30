7e4bdd231dc0ff232b0a40a8ae1db1cf99c00656
Phil Benford <phillipbenford@gmail.com>
2021-09-30 22:54:58 +0100

    commit

--- ticTacToe.py
+++ ticTacToe.py
@@ -18,21 +18,70 @@ board = [[" ","1","2","3"],["1","_","_","_"],["2","_","_","_"],["3","_","_","_"]
 game = True
 
 while game: # Whilst the game is True (as set above) continue the loop
-    print(" ***** Board *****") 
+    print(" \n***** Board *****")
     for i in board: # Outputs a 2D board created by the list above (board)
         print(i)
-    
-    row_select = int(input("\nPlease select a row #: ")) # Prompts user to select a row and store input as an integer
-    while row_select < 1 or row_select > 3: # Checks user input is valid, if not prompt user to enter a valid number 
-        row_select = int(input("Please select a valid row between (1-3): "))
-    col_select = int(input("Please select a column #: "))
-    while col_select < 1 or col_select > 3:
-        col_select = int(input("Please select a valid column between (1-3): "))
-    player_choice = input("Please choose, x, o, or q to quit: ")
+    valid = ["1", "2", "3"]
+    x_o_q = ["x", "o", "q"]
 
-    # Checks the input from player_choice, if "q" end game, else store the value in the list
-    if player_choice == "q": 
-        print("\nThanks for playing!","\nGoodbye" )
-        game = False
-    else: 
-        board[row_select][col_select] = player_choice 
+    for a,s,d,f in board:
+
+        if s and d and f == x_o_q[0]:
+            print("\nWinner Winner Chicken Dinner!")
+            print("\nThanks for playing!", "\nGoodbye")
+            game = False
+            break
+        elif s and d and f == x_o_q[1]:
+            print("\nWinner Winner Chicken Dinner!")
+            print("\nThanks for playing!", "\nGoodbye")
+            game = False
+            break
+
+    validate_row = True
+    while validate_row:
+        row = input("\nPlease select a row #: ")
+        for i in valid:
+            if i == row[0]:
+                row = int(row)
+                validate_row = False
+                break
+        else:
+            print("Please select a valid row between (1-3)!")
+
+    validate_col = True
+    while validate_col:
+        col = input("\nPlease select a column #: ")
+        for i in valid:
+            if i == col[0]:
+                col = int(col)
+                validate_col = False
+                break
+        else:
+            print("Please select a valid col between (1-3)!")
+
+    validate_choice = True
+    while validate_choice:
+        player_choice = input("\nPlease choose, x, o, or q to quit: ")
+        for i in x_o_q:
+            if player_choice[0] == x_o_q[-1]:
+                print("\nThanks for playing!", "\nGoodbye")
+                game = False
+                validate_choice = False
+                break
+            elif i == player_choice[0]:
+                free_space = True
+                while free_space:
+                    for x in x_o_q:
+                        if board[row][col] != x:
+                            board[row][col] = player_choice
+                            validate_choice = False
+                            free_space = False
+                            break
+                        else:
+                            print("\nTry again that space is taken!")
+                            free_space = False
+                            validate_choice = False
+                            break
+                break
+        else:
+            print("Invalid character!")
